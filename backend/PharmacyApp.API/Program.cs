557e1164364463d8d943340de5773abba41b9a0d
websiteeyyay-tech <websiteeyyay@gmail.com>
2025-10-18 03:45:23 +0000

    ya

--- backend/PharmacyApp.API/Program.cs
+++ backend/PharmacyApp.API/Program.cs
@@ -4,32 +4,39 @@ using PharmacyApp.Infrastructure.Data;
 using PharmacyApp.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure Database (InMemory for now)
 builder.Services.AddDbContext<PharmacyDbContext>(options =>
-    options.UseInMemoryDatabase("PharmacyDb"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Dependency Injection setup
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IReportService, ReportService>();
 
 var app = builder.Build();
 
+// Configure middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.Urls.Add("http://localhost:5272");
+app.Urls.Add("https://localhost:7114");
+
+
 app.UseHttpsRedirection();
 app.MapControllers();
-app.Run();
 
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IReportService, ReportService>();
+
+app.Run();
