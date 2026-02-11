239782b7d123b891e98427a7a85fdfe56b9524c0
LiamSku <websiteeyyay@gmail.com>
2026-02-11 03:49:58 +0000

    MUKA KA KASING PERAAA HAHAHAHHAHAHAHAHAHA

--- backend/PharmacyApp.Core/Interfaces/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-using PharmacyApp.Core.Entities;
-
-namespace PharmacyApp.Core.Interfaces
-{
-    public interface IProductRepository : IGenericRepository<Product>
-    {
-        // add product-specific repository methods here if needed
-    }
-}
--- backend/PharmacyApp.Core/Interfaces/IReportService.cs
+++
@@ -1,8 +0,0 @@
-namespace PharmacyApp.Core.Interfaces
-{
-    public interface IReportService
-    {
-        object GetSalesSummary();
-        object GetInventoryStatus();
-    }
-}
